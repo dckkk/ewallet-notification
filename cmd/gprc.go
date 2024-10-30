a2ab3fde418869ad2eff2560e668a1fdf368950f
Dicky Pratama <dicky.pratama@arthasemesta.com>
2024-10-31 00:52:23 +0700

    add dockerfile

--- cmd/gprc.go
+++ cmd/gprc.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"ewallet-notification/cmd/proto/notification"
 	"ewallet-notification/helpers"
+	"ewallet-notification/internal/api"
+	"ewallet-notification/internal/repository"
+	"ewallet-notification/internal/services"
 	"log"
 	"net"
 
@@ -10,12 +14,15 @@ import (
 )
 
 func ServeGRPC() {
+	d := dependencyInject()
+
 	lis, err := net.Listen("tcp", ":"+helpers.GetEnv("GRPC_PORT", "7000"))
 	if err != nil {
 		log.Fatal("failed to listen grpc port: ", err)
 	}
 
 	s := grpc.NewServer()
+	notification.RegisterNotificationServiceServer(s, d.EmailAPI)
 
 	// list method
 	// pb.ExampleMethod(s, &grpc....)
@@ -23,5 +30,27 @@ func ServeGRPC() {
 	logrus.Info("start listening grpc on port:" + helpers.GetEnv("GRPC_PORT", "7000"))
 	if err := s.Serve(lis); err != nil {
 		log.Fatal("failed to serve grpc port: ", err)
+	}
+}
+
+type Dependency struct {
+	EmailAPI *api.EmailAPI
+}
+
+func dependencyInject() *Dependency {
+	emailRepo := &repository.EmailRepo{
+		DB: helpers.DB,
+	}
+
+	emailSvc := &services.EmailService{
+		EmailRepo: emailRepo,
+	}
+
+	emailAPI := &api.EmailAPI{
+		EmailService: emailSvc,
+	}
+
+	return &Dependency{
+		EmailAPI: emailAPI,
 	}
 }
